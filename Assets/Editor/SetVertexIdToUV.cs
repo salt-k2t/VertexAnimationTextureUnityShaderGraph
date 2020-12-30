aa6475f4af87e622a9f9ef3efb0963b8d5a5a63c
salt-k2t <ys.yrkt@gmail.com>
2020-12-30 16:49:35 +0900

    Initial implement

---
+++ Assets/Editor/SetVertexIdToUV.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEditor;
+
+
+public class SetVertexIdToUV : EditorWindow
+{
+    private Mesh target;
+
+    [MenuItem("Tools/Open SetVertexIdToUV2")]
+    private static void Open()
+    {
+        var editorWindow = EditorWindow.GetWindow(typeof(SetVertexIdToUV)) as SetVertexIdToUV;
+        editorWindow.Show();
+    }
+
+    private void OnGUI()
+    {
+        target = EditorGUILayout.ObjectField("Target Mesh", target, typeof(Mesh), false) as Mesh;
+
+        if (GUILayout.Button("Bake"))
+        {
+            Bake();
+        }
+    }
+
+    private void Bake()
+    {
+        int vertexCount = target.vertexCount;
+        var uv2 = new Vector2[vertexCount];
+        for (int i = 0; i < vertexCount; i++)
+        {
+            uv2[i] = new Vector2(i, i);
+        }
+
+        target.uv2 = uv2;
+        EditorUtility.SetDirty(target);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # VertexAnimationTextureUnityShaderGraph
+テクスチャにベイクした頂点アニメーションを実行するシェーダーサンプルです。  
+ShaderGraphで使用する事ができます。  
+**実装途中(WIP)です**
+
+## Material設定
+- VertexAnimationTexture  
+Mayaツールで作成した頂点アニメーションテクスチャをセットします。
+- Detail  
+Mayaツールで設定したDetailを入力してください。
+
+## テクスチャ設定
+- sRGB (Color Texture) : Off  
+- Non-Power of 2 : None  
+- Generate Mip Maps : Off  
+- FilterNode : Point(no filter)
+- Format : RGBA 32 bit
+
+## 注意
+
+頂点番号はUV2から取得しています。
+Tools→Open SetVertexIdToUV2にて格納処理を行ってください。
+(ShaderGraphではVertexIdを取得できない?)
+
+テクスチャサイズはプロパティとして設定しています。
+(Texel Size NodeがVertexPositionに接続できない?)
